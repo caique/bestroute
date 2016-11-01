cafdbf2442687a3ec74600cbf75ec0faf1b34123
Caique Peixoto <caiquepeixoto1@gmail.com>
2016-11-01 01:19:45 -0200

    Tests updated

--- src/test/java/br/ignicaodigital/bestroute/domain/LocationTest.java
+++ src/test/java/br/ignicaodigital/bestroute/domain/LocationTest.java
@@ -74,15 +74,6 @@ public class LocationTest {
 		Location origin = Location.at(0, 0);
 		Location target = Location.at(1, 0);;
 		assertEquals(1.0, Location.calculateTimeBetween(origin, target, 1), 0);
-	}
-	
-	@Test
-	public void vazio(){
-		Matcher matcher = Pattern.compile("(\\d+,\\d+)").matcher("0,0:0,1");
-		if(matcher.find()){
-			System.out.println(matcher.group(1));
-			System.out.println(matcher.group(2));
-		}
-	}
+	}	
 	
 }
